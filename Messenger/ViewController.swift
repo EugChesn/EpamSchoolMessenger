1ab0bb361be2dde1d77cab478e50ffc798d96282
Anastasia-Demidova <60142958+Anastasia-Demidova@users.noreply.github.com>
2020-03-06 13:07:30 +0300

    Initial Commit

---
+++ Messenger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Анастасия Демидова on 06.03.2020.
+//  Copyright © 2020 Anastasia Demidova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
