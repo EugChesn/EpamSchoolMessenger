2e666d78ff32e2cb8e37840b3e62ee32f50f9084
Evgeny Gusev <wackyy52@gmail.com>
2020-03-11 22:36:41 +0300

    ---

--- Messenger/Scenes/Login/SignIn/SignInViewModel.swift
+++ Messenger/Scenes/Login/SignIn/SignInViewModel.swift
@@ -11,19 +11,5 @@ import Foundation
 
 
 class SignInViewModel {
-    
-    var autorizeModel: AutorizeModel?
-    
-    func login(number: String, password: String) -> Bool {
-        autorizeModel = AutorizeModel(phone: number, password: password)
-        
-        guard let model = autorizeModel else { return false }
-        return model.autorize()
-    }
-    
-    func getToken() -> UserToken {
-        guard let token = autorizeModel?.getUserToken()
-            else { return UserToken(phone: "",token: "") }
-        return token
-    }
+
 }
