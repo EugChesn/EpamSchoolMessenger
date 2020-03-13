8e72e07e3cfabdad353887a19659e6b05f6075a7
Evgeny Gusev <wackyy52@gmail.com>
2020-03-13 10:18:37 +0300

    Add files for network and storage services

---
+++ Messenger/Servicies/StorageService.swift
@@ -0,0 +1,17 @@
+//
+//  StorageService.swift
+//  Messenger
+//
+//  Created by Евгений Гусев on 13.03.2020.
+//  Copyright © 2020 Anastasia Demidova. All rights reserved.
+//
+
+import Foundation
+
+protocol StorageData {
+    
+}
+
+class StorageService: StorageData {
+    
+}
