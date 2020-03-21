3e7ae897343bd5c30c189f99f1bc65946b51b780
maccadley <>
2020-03-21 19:20:40 +0300

    fixed UI, started chats' list

---
+++ Messenger/Messenger/Scenes/Messenger/Chats/ChatsList/ChatListCell/ChatsListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ChatsListTableViewCell.swift
+//  Messenger
+//
+//  Created by Admin on 21.03.2020.
+//  Copyright © 2020 Anastasia Demidova. All rights reserved.
+//
+
+import UIKit
+
+class ChatsListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Messenger/Messenger/Scenes/Messenger/Chats/ChatsList/Utilities/ChatListUtilities.swift
@@ -0,0 +1,9 @@
+//
+//  ChatListUtilities.swift
+//  Messenger
+//
+//  Created by Admin on 21.03.2020.
+//  Copyright © 2020 Anastasia Demidova. All rights reserved.
+//
+
+import Foundation
