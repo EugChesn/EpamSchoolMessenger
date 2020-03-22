7692e972f518117403eeecdfcf819a22f7ccfe15
Anastasia-Demidova <60142958+Anastasia-Demidova@users.noreply.github.com>
2020-03-22 03:18:21 +0300

    Added SearchBar with "Cancel" button

--- Messenger/Scenes/Messenger/Settings/SettingsViewController.swift
+++ Messenger/Scenes/Messenger/Settings/SettingsViewController.swift
@@ -17,16 +17,48 @@ class SettingsViewController: UIViewController {
     var viewModel: SettingsViewModeling?
     var router: SettingsRouting?
     
+    private enum Title {
+        static let setting = "Settings"
+        static let rightBarButtonItem = "Edit"
+        static let search = "Search"
+    }
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        navigationItem.rightBarButtonItem?.title = Title.rightBarButtonItem
+        searchBar.placeholder = Title.search
+        searchBar.delegate = self
         setupDependencies()
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        navigationItem.title = Title.setting
+    }
     
     func setupDependencies() {
         viewModel = SettingsViewModel(view: self)
         router = SettingsRouter(viewController: self)
+    }
+}
+
+extension SettingsViewController: UISearchBarDelegate {
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        searchBar.setShowsCancelButton(true, animated: true)
+    }
+
+    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+        searchBar.setShowsCancelButton(false, animated: true)
+    }
+
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        view.endEditing(true)
+    }
+
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        view.endEditing(true)
+        searchBar.searchTextField.text = nil
     }
 }
 
