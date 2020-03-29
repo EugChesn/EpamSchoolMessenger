e836e7bb4a851ea48f4878ae3ac31d6b86c2ebd8
Evgeny Gusev <wackyy52@gmail.com>
2020-03-29 19:01:32 +0300

    Simple UI For ChatList

--- Messenger/Scenes/Messenger/Chats/ChatsList/ChatsViewController.swift
+++ Messenger/Scenes/Messenger/Chats/ChatsList/ChatsViewController.swift
@@ -10,26 +10,65 @@ import Foundation
 import UIKit
 
 protocol ChatsDelegate: class {
-    
+    func updateChats()
 }
 
 class ChatsViewController: UIViewController {
-    var viewModel: ChatsViewModeling?
+    var viewModel: ChatsViewModeling!
     var router: ChatsRouting?
+    
+    @IBOutlet weak var chatsTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupUI()
         setupDependencies()
     }
-    
     
     func setupDependencies() {
         viewModel = ChatsViewModel(view: self)
         router = ChatsRouter(viewController: self)
     }
+    
+    func setupUI() {
+        chatsTableView.delegate = self
+        chatsTableView.dataSource = self
+        
+        let searchConroller = UISearchController(searchResultsController: nil)
+        searchConroller.searchBar.delegate = self
+        navigationItem.searchController = searchConroller
+    }
 }
 
 extension ChatsViewController: ChatsDelegate {
+    func updateChats() {
+        DispatchQueue.main.async {
+            self.chatsTableView.reloadData()
+        }
+    }
+}
+
+extension ChatsViewController: UISearchBarDelegate {
+    
+}
+
+extension ChatsViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.chatsCount
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "chatCellId")
+        
+        let chat = viewModel.getChat(atIndex: indexPath.row)
+        
+        cell.textLabel?.text = chat.name
+        cell.detailTextLabel?.text = chat.lastMessage
+        cell.imageView?.image = UIImage(named: "profile")
+        
+        return cell
+    }
+    
     
 }
--- Messenger/Scenes/Messenger/Chats/ChatsList/ChatsViewModel.swift
+++ Messenger/Scenes/Messenger/Chats/ChatsList/ChatsViewModel.swift
@@ -7,15 +7,50 @@
 //
 
 import Foundation
+import UIKit
 
 protocol ChatsViewModeling {
+    var chatsCount: Int {get}
     
+    func getChat(atIndex: Int) -> Chat
 }
 
 class ChatsViewModel: ChatsViewModeling {
+    
     weak var view: ChatsDelegate?
+    
+    var chatsList: [Chat] = []
+    
+    var chatsCount: Int {
+        get {
+            return chatsList.count
+        }
+    }
+    
+    func getChat(atIndex: Int) -> Chat {
+        let chat = Chat()
+        return chat
+    }
     
     init(view: ChatsDelegate) {
         self.view = view
+        let chat = Chat()
+ 
+        chatsList.append(chat)
+        chatsList.append(chat)
+        chatsList.append(chat)
+    }
+    
+    private func downLoadChats() -> [Chat] {
+
+        return []
     }
 }
+
+struct Chat {
+    var lastMessage: String = "test"
+    var uid: String = "uid"
+    var name: String = "name"
+    var profileImage: UIImage?
+}
+
