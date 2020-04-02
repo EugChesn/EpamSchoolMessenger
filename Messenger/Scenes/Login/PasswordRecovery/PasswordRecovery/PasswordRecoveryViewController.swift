178704acb1edc73ad15ca0292bc141dce4bc33ac
maccadley <>
2020-04-02 18:40:54 +0300

    Reset pass added

--- Messenger/Scenes/Login/PasswordRecovery/PasswordRecovery/PasswordRecoveryViewController.swift
+++ Messenger/Scenes/Login/PasswordRecovery/PasswordRecovery/PasswordRecoveryViewController.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import UIKit
+import FirebaseAuth
 
 protocol PasswordRecoveryDelegate: class {
     
@@ -16,6 +17,8 @@ protocol PasswordRecoveryDelegate: class {
 class PasswordRecoveryViewController: UIViewController {
     var viewModel: PasswordRecoveryViewModeling?
     var router: PasswordRecoveryRouting?
+    @IBOutlet weak var emailInputText: UITextField!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,6 +31,26 @@ class PasswordRecoveryViewController: UIViewController {
         viewModel = PasswordRecoveryViewModel(view: self)
         router = PasswordRecoveryRouter(viewController: self)
     }
+    
+    @IBAction func resetButtonPressed(_ sender: UIButton) {
+        guard let email = emailInputText.text else {
+            alertError(errorCode: .invalidEmail)
+            return
+        }
+        
+        Auth.auth().sendPasswordReset(withEmail: email) { (error) in
+            if let err = error{
+                self.alertError(errorCode: .invalidEmail)
+                print(err)
+            }
+        }
+        let alert = UIAlertController(title: "Mail Sent", message: "We have just sent you a password reset e-mail. Please, check your inbox and follow the instructions", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        alert.addAction(okAction)
+        self.present(alert, animated: true, completion: nil)
+        self.dismiss(animated: true, completion: nil)
+    }
+    
 }
 
 extension PasswordRecoveryViewController: PasswordRecoveryDelegate {
