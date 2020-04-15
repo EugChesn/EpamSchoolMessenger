7dd7eb6f6446473b6ad2b04270c46dfe18513750
Anastasia-Demidova <60142958+Anastasia-Demidova@users.noreply.github.com>
2020-04-15 14:56:08 +0300

    ProfileTableViewCell

---
+++ Messenger/Scenes/Messenger/Settings/ProfileTableViewCell/ProfileTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  ProfileTableViewCell.swift
+//  Messenger
+//
+//  Created by Анастасия Демидова on 01.04.2020.
+//  Copyright © 2020 Anastasia Demidova. All rights reserved.
+//
+
+import UIKit
+
+class ProfileTableViewCell: UITableViewCell {
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        if profileImage != nil {
+            profileImage.roundWithBorder()
+        }
+    }
+}
