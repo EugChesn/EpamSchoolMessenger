77ee8f41b9790394333f6de15df57ab61636b473
Eugene Chistyakov <chesnok297@gmail.com>
2020-04-16 18:54:22 +0300

    Fix color textfield

--- Messenger/Scenes/Login/Utilities/Utilities.swift
+++ Messenger/Scenes/Login/Utilities/Utilities.swift
@@ -18,8 +18,8 @@ class Utilities {
     static func styleTextField(_ textfield: UITextField) {
         textfield.borderStyle = .none
         let border = CALayer()
-        let width = CGFloat(1.0)
-        border.borderColor = UIColor.blue.cgColor
+        let width = CGFloat(2.0)
+        border.borderColor = UIColor.systemBlue.cgColor
         border.frame = CGRect(x: 0, y: textfield.frame.size.height - width,
                               width:  textfield.frame.size.width, height: textfield.frame.size.height)
         border.borderWidth = width
@@ -35,13 +35,7 @@ class Utilities {
         imageView.layer.cornerRadius = imageView.frame.size.height / 2
         imageView.contentMode = .scaleAspectFill
         imageView.layer.masksToBounds = true
-        imageView.layer.borderWidth = 1
+        imageView.layer.borderWidth = 2
         imageView.layer.borderColor = UIColor.lightGray.cgColor
     }
-    /*static func styleTextFieldRadius(_ textfield: UITextField) {
-        textfield.layer.masksToBounds = true
-        textfield.layer.cornerRadius = textfield.frame.height / 2
-        textfield.layer.borderWidth = 1
-        textfield.layer.borderColor = UIColor.black.cgColor
-    }*/
 }
